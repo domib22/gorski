9fc50964762394518c5b592cc4098cd5899d155a
domib22 <domib22@users.noreply.github.com>
2020-12-01 20:04:17 +0100

    Initial commit

---
+++ src/main/java/com/example/gorski/GorskiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gorski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GorskiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GorskiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gorski/GorskiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gorski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GorskiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
