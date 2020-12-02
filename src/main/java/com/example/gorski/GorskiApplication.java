4f411a24f3aa024b3577c7c8e11ff52b2a8107af
domib22 <domib22@users.noreply.github.com>
2020-12-02 16:58:43 +0100

    Add User.java and UserRepository.java +configuration H2 console

--- src/main/java/com/example/gorski/GorskiApplication.java
+++ src/main/java/com/example/gorski/GorskiApplication.java
@@ -1,13 +1,28 @@
 package com.example.gorski;
 
+import com.example.gorski.domain.User;
+import com.example.gorski.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GorskiApplication {
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GorskiApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+//            Save demo data after start
+			userRepository.save(new User("User", "user", "male"));
+		};
+	}
 }
