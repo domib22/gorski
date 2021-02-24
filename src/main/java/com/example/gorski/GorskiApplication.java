ae92c3efa91c6bff2c20db1e695f7e19554c1537
domib22 <domib22@users.noreply.github.com>
2021-02-24 17:20:47 +0100

    Flyway configuration Add V2 migration (user roles)

--- src/main/java/com/example/gorski/GorskiApplication.java
+++ src/main/java/com/example/gorski/GorskiApplication.java
@@ -1,31 +1,26 @@
 package com.example.gorski;
 
-import com.example.gorski.domain.users.User;
-import com.example.gorski.domain.users.UserGender;
-import com.example.gorski.domain.users.UserRepository;
+import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
-public class GorskiApplication {
+public class GorskiApplication implements CommandLineRunner {
 
 	@Autowired
-	private UserRepository userRepository;
+	DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GorskiApplication.class, args);
 	}
 
-	@Bean
-	CommandLineRunner runner() {
-		return args -> {
-//            Save demo data after start
-			userRepository.save(new User("User", "$2y$12$Gsf7AIAc0xNnVCnzlVhePeSuFIgmC1I.jS0v7ERVQtkfBmjGYvZ7m", UserGender.WOMAN));
-			userRepository.save(new User("User2", "$2y$12$cP0eQoLH.os1IxYFI/PIIupYhNJ3fpOyrYMIXZJ1rnuMyXbMX333W", UserGender.MAN));
-		};
-
+	@Override
+	public void run(String... args) throws Exception {
+		Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
 	}
+
 }
--- src/main/java/com/example/gorski/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/gorski/configuration/SecurityConfiguration.java
@@ -16,11 +16,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -58,7 +53,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable().
                 authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/app/auth/**").permitAll()
+                .antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
@@ -66,20 +62,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         http.headers().frameOptions().disable();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
-        config.setAllowedMethods(Arrays.asList("*"));
-        config.setAllowedHeaders(Arrays.asList("*"));
-        config.setAllowCredentials(true);
-        config.applyPermitDefaultValues();
-
-        source.registerCorsConfiguration("/**", config);
-        return source;
     }
 
 }
