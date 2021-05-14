cadb048f3fbf925bec366cdd931cbf536ea9ff01
domib22 <domib22@users.noreply.github.com>
2021-05-14 13:35:07 +0200

    Add test data migrations V5-V6

---
+++ src/main/resources/db/migration/V6__Add_Test_UserProducts.sql
@@ -0,0 +1,4 @@
+INSERT INTO user_products(user_id, product_id) VALUES(2, 3);
+INSERT INTO user_products(user_id, product_id) VALUES(2, 6);
+INSERT INTO user_products(user_id, product_id) VALUES(2, 10);
+INSERT INTO user_products(user_id, product_id) VALUES(2, 30);
